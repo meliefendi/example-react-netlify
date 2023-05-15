148e17bbb117b5d15c95592e4973be8291008ecf
Melih <melh_akkas@hotmail.com>
2023-05-15 15:52:25 +0300

    link

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Learn React
-       npm 
-       package 
-      </header>
+    <div className="App ">
+      <h1>Melih Akkas</h1>
+      <br />
+
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <a href='https://github.com/meliefendi' className="btn btn-secondary" type="button">Githup</a>
+        <a href="https://www.facebook.com/melh.akkas/" className="btn btn-primary" type="button">facebook</a>
+        <a href='https://twitter.com/win12pssp' className="btn btn-warning" type="button">Twitter</a>
+        <a href='https://www.instagram.com/meli.akkas/' className="btn btn-danger" type="button">İnstagram</a>
+        <a href='https://medium.com/@melh_akkas' className="btn btn-dark" type="button">Medium</a>
+        <a href='https://www.linkedin.com/in/melih-akkas-a1791a16b/' className="btn btn-info" type="button">Linkedln</a>
+        <button className="btn btn-success" type="button">Button</button>
+      </div>
     </div>
   );
 }
