1c4e58f7e0b3109fd793422179db1e951fe8e468
Melih <melh_akkas@hotmail.com>
2023-05-15 15:53:54 +0300

    link2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="d-grid gap-2 col-6 mx-auto">
         <a href='https://github.com/meliefendi' className="btn btn-secondary" type="button">Githup</a>
         <a href="https://www.facebook.com/melh.akkas/" className="btn btn-primary" type="button">facebook</a>
-        <a href='https://twitter.com/win12pssp' className="btn btn-warning" type="button">Twitter</a>
+        <a href='https://twitter.com/win12pssp' className="btn btn-info" type="button">Twitter</a>
         <a href='https://www.instagram.com/meli.akkas/' className="btn btn-danger" type="button">İnstagram</a>
         <a href='https://medium.com/@melh_akkas' className="btn btn-dark" type="button">Medium</a>
         <a href='https://www.linkedin.com/in/melih-akkas-a1791a16b/' className="btn btn-info" type="button">Linkedln</a>
